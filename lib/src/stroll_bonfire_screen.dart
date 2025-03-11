906fbf1023d5d8239a696899c10ca37ee06870d9
EmmyKay0026 <kadiriemmanueloshio0026@gmail.com>
2025-03-11 12:41:58 +0100

    updated UI

--- lib/src/stroll_bonfire_screen.dart
+++ lib/src/stroll_bonfire_screen.dart
@@ -29,25 +29,12 @@ class _StrollBonfirePageState extends State<StrollBonfirePage> {
     return Stack(
       fit: StackFit.expand,
       children: [
-        Column(
-          children: [
-            // SvgPicture.asset("assets/images/backgroundvideo.svg"),
-            Image.asset(
-              "assets/images/video.png",
-              fit: BoxFit.cover,
-            ),
-            Expanded(
-              child: Container(
-                decoration: BoxDecoration(
-                  gradient: LinearGradient(
-                    colors: [Color(0xff000000), Color(0xff000000)],
-                    begin: Alignment.topCenter,
-                    end: Alignment.bottomCenter,
-                  ),
-                ),
-              ),
-            ),
-          ],
+        SizedBox(
+          height: MediaQuery.of(context).size.height,
+          child: Image.asset(
+            "assets/images/video.png",
+            fit: BoxFit.cover,
+          ),
         ),
         SafeArea(
           child: Column(
@@ -55,7 +42,7 @@ class _StrollBonfirePageState extends State<StrollBonfirePage> {
               // App bar
               Padding(
                 padding:
-                    const EdgeInsets.symmetric(horizontal: 16.0, vertical: 8.0),
+                    const EdgeInsets.symmetric(horizontal: 16.0, vertical: .0),
                 child: Row(
                   mainAxisAlignment: MainAxisAlignment.center,
                   children: [
@@ -67,9 +54,9 @@ class _StrollBonfirePageState extends State<StrollBonfirePage> {
                         color: Color(0xFFCCC8FF),
                         shadows: const [
                           Shadow(
-                            blurRadius: 10.0,
-                            color: Colors.black26,
-                            offset: Offset(0, 2),
+                            blurRadius: 4.0,
+                            color: Color(0xffBEBEBE),
+                            offset: Offset(0, 1),
                           ),
                         ],
                       ),
@@ -89,38 +76,21 @@ class _StrollBonfirePageState extends State<StrollBonfirePage> {
                 child: Row(
                   mainAxisAlignment: MainAxisAlignment.center,
                   children: [
-                    Container(
-                      padding: const EdgeInsets.symmetric(
-                          horizontal: 8, vertical: 2),
-                      decoration: BoxDecoration(
-                        color: Colors.black.withOpacity(0.2),
-                        borderRadius: BorderRadius.circular(10),
-                      ),
-                      child: Row(
-                        children: const [
-                          Icon(Icons.access_time,
-                              color: Colors.white, size: 16),
-                          SizedBox(width: 4),
-                          Text('22h 00m',
-                              style: TextStyle(color: Colors.white)),
-                        ],
-                      ),
+                    Row(
+                      children: const [
+                        Icon(Icons.access_time, color: Colors.white, size: 16),
+                        SizedBox(width: 4),
+                        Text('22h 00m', style: TextStyle(color: Colors.white)),
+                      ],
                     ),
                     const SizedBox(width: 12),
-                    Container(
-                      padding: const EdgeInsets.symmetric(
-                          horizontal: 8, vertical: 2),
-                      decoration: BoxDecoration(
-                        color: Colors.black.withOpacity(0.2),
-                        borderRadius: BorderRadius.circular(10),
-                      ),
-                      child: Row(
-                        children: const [
-                          Icon(Icons.people, color: Colors.white, size: 16),
-                          SizedBox(width: 4),
-                          Text('103', style: TextStyle(color: Colors.white)),
-                        ],
-                      ),
+                    Row(
+                      children: const [
+                        Icon(Icons.person_outlined,
+                            color: Colors.white, size: 16),
+                        SizedBox(width: 4),
+                        Text('103', style: TextStyle(color: Colors.white)),
+                      ],
                     ),
                   ],
                 ),
@@ -132,7 +102,6 @@ class _StrollBonfirePageState extends State<StrollBonfirePage> {
                 padding: const EdgeInsets.all(16.0),
                 child: Column(
                   children: [
-                    const SizedBox(height: 16),
                     QuestionWidget(
                         initialQuestion: _currentQuestion,
                         onOptionSelected: (selectedOptionId) {
@@ -141,44 +110,47 @@ class _StrollBonfirePageState extends State<StrollBonfirePage> {
                           });
                         }),
                     Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
                       children: [
                         Text(
-                          'Pick your option. \n See who has a similar mind.',
+                          'Pick your option. \nSee who has a similar mind.',
                           style:
-                              TextStyle(color: Color(0xffE5E5E5), fontSize: 14),
+                              TextStyle(color: Color(0xffE5E5E5), fontSize: 12),
                         ),
-                        Spacer(),
-                        Container(
-                          padding:
-                              EdgeInsets.symmetric(vertical: 9, horizontal: 13),
-                          decoration: BoxDecoration(
-                            border: Border.all(color: AppColors.primaryPurple),
-                            shape: BoxShape.circle,
-                          ),
-                          child: Icon(
-                            Icons.mic,
-                            color: AppColors.primaryPurple,
-                            size: 35,
-                          ),
+                        Row(
+                          children: [
+                            Container(
+                              padding: EdgeInsets.symmetric(
+                                  vertical: 4, horizontal: 13),
+                              decoration: BoxDecoration(
+                                border:
+                                    Border.all(color: AppColors.primaryPurple),
+                                shape: BoxShape.circle,
+                              ),
+                              child: Icon(
+                                Icons.mic,
+                                color: AppColors.primaryPurple,
+                                size: 26,
+                              ),
+                            ),
+                            Container(
+                              padding: EdgeInsets.symmetric(
+                                  vertical: 4, horizontal: 13),
+                              decoration: BoxDecoration(
+                                color: AppColors.primaryPurple,
+                                // border: Border.all(color: AppColors.primaryPurple),
+                                shape: BoxShape.circle,
+                              ),
+                              child: Icon(
+                                Icons.arrow_forward,
+                                color: Color(0xff020202),
+                                size: 26,
+                              ),
+                            )
+                          ],
                         ),
-                        Spacer(),
-                        Container(
-                          padding:
-                              EdgeInsets.symmetric(vertical: 9, horizontal: 13),
-                          decoration: BoxDecoration(
-                            color: AppColors.primaryPurple,
-                            // border: Border.all(color: AppColors.primaryPurple),
-                            shape: BoxShape.circle,
-                          ),
-                          child: Icon(
-                            Icons.arrow_forward,
-                            color: Color(0xff020202),
-                            size: 35,
-                          ),
-                        )
                       ],
                     ),
-                    const SizedBox(height: 8),
                   ],
                 ),
               ),
@@ -189,42 +161,3 @@ class _StrollBonfirePageState extends State<StrollBonfirePage> {
     );
   }
 }
-
-// Widget _buildOptionButton(String option, String content, bool isSelected) {
-//   return GestureDetector(
-//     child: Expanded(
-//       child: Container(
-//         padding: const EdgeInsets.symmetric(vertical: 12, horizontal: 12),
-//         decoration: BoxDecoration(
-//           color: Colors.black.withOpacity(0.6),
-//           borderRadius: BorderRadius.circular(8),
-//         ),
-//         child: Row(
-//           children: [
-//             Container(
-//               width: 24,
-//               height: 24,
-//               decoration: BoxDecoration(
-//                 shape: BoxShape.circle,
-//                 border: Border.all(color: Colors.white),
-//               ),
-//               child: const Center(
-//                 child: Text(
-//                   'C',
-//                   style: TextStyle(color: Colors.white),
-//                 ),
-//               ),
-//             ),
-//             const SizedBox(width: 8),
-//             const Expanded(
-//               child: Text(
-//                 'Wind-down time after dinners',
-//                 style: TextStyle(color: Colors.white),
-//               ),
-//             ),
-//           ],
-//         ),
-//       ),
-//     ),
-//   );
-// }
